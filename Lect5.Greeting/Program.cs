cb1ece06501f1c7bcbf59513ea81e777d3cc6d12
JuliyaBva <yuliyab8508@gmail.com>
2020-03-18 00:18:14 +0300

    commit task1

---
+++ Lect5.Greeting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Lect5.Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan dt = DateTime.Now.TimeOfDay;
+            int hours = dt.Hours;
+            if (hours >= 9 && hours < 12)
+            {
+                Console.WriteLine("Good morning, guys!");
+            }
+            else if (hours >= 12 && hours < 15)
+            {
+                Console.WriteLine("Good day, guys!");
+            }
+            if (hours >= 15 && hours < 22)
+            {
+                Console.WriteLine("Good evening, guys!");
+            }
+        }
+    }
+}
